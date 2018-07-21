acad98668da2ec256d1794c2f48d476b0278b314
吴超宝 <wucb@yuntongxun.com>
2018-07-21 15:57:21 +0800

    init project

---
+++ hp_core/src/test/java/com/orgxw/hp_core/HpCoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.orgxw.hp_core;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HpCoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
